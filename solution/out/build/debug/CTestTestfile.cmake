ed334859abfbacb80f8157a76cf0d102561f20e9
artemijr <arrtemijr@gmail.com>
2023-10-30 02:48:06 +0300

    ABOBA

---
+++ solution/out/build/debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/CProjects/assignment-image-rotation1
+# Build directory: C:/CProjects/assignment-image-rotation1/out/build/debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("solution")
+subdirs("tester")
---
+++ solution/out/build/release/tester/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: C:/CProjects/assignment-image-rotation1/tester
+# Build directory: C:/CProjects/assignment-image-rotation1/out/build/release/tester
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-1 "C:/Program Files/JetBrains/CLion 2023.2.2/bin/cmake/win/x64/bin/cmake.exe" "-DTEST_DIR=C:/CProjects/assignment-image-rotation1/tester/tests/1" "-DIMAGE_TRANSFORMER=C:/CProjects/assignment-image-rotation1/out/build/release/solution/image-transformer.exe" "-DIMAGE_MATCHER=C:/CProjects/assignment-image-rotation1/out/build/release/tester/image-matcher.exe" "-P" "C:/CProjects/assignment-image-rotation1/tester/tester.cmake")
+set_tests_properties(test-1 PROPERTIES  _BACKTRACE_TRIPLES "C:/CProjects/assignment-image-rotation1/tester/CMakeLists.txt;16;add_test;C:/CProjects/assignment-image-rotation1/tester/CMakeLists.txt;0;")
+add_test(test-2 "C:/Program Files/JetBrains/CLion 2023.2.2/bin/cmake/win/x64/bin/cmake.exe" "-DTEST_DIR=C:/CProjects/assignment-image-rotation1/tester/tests/2" "-DIMAGE_TRANSFORMER=C:/CProjects/assignment-image-rotation1/out/build/release/solution/image-transformer.exe" "-DIMAGE_MATCHER=C:/CProjects/assignment-image-rotation1/out/build/release/tester/image-matcher.exe" "-P" "C:/CProjects/assignment-image-rotation1/tester/tester.cmake")
+set_tests_properties(test-2 PROPERTIES  _BACKTRACE_TRIPLES "C:/CProjects/assignment-image-rotation1/tester/CMakeLists.txt;16;add_test;C:/CProjects/assignment-image-rotation1/tester/CMakeLists.txt;0;")
+add_test(test-3 "C:/Program Files/JetBrains/CLion 2023.2.2/bin/cmake/win/x64/bin/cmake.exe" "-DTEST_DIR=C:/CProjects/assignment-image-rotation1/tester/tests/3" "-DIMAGE_TRANSFORMER=C:/CProjects/assignment-image-rotation1/out/build/release/solution/image-transformer.exe" "-DIMAGE_MATCHER=C:/CProjects/assignment-image-rotation1/out/build/release/tester/image-matcher.exe" "-P" "C:/CProjects/assignment-image-rotation1/tester/tester.cmake")
+set_tests_properties(test-3 PROPERTIES  _BACKTRACE_TRIPLES "C:/CProjects/assignment-image-rotation1/tester/CMakeLists.txt;16;add_test;C:/CProjects/assignment-image-rotation1/tester/CMakeLists.txt;0;")
